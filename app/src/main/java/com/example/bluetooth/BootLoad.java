9884fb4e25acd6f8621c763aaf1feb9131116527
GerraAyush <ayushgerra3@gmail.com>
2023-06-28 19:46:47 +0530

    My new commit

--- app/src/main/java/com/example/bluetooth/BootLoad.java
+++ app/src/main/java/com/example/bluetooth/BootLoad.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,11 +15,14 @@ import java.util.TimerTask;
 
 public class BootLoad extends AppCompatActivity {
 
+    ImageView logoImageTop, logoImageBottom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boot_load);
 
+        /*
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
@@ -24,7 +31,43 @@ public class BootLoad extends AppCompatActivity {
                 startActivity(intent);
             }
         };
-
         timer.schedule(task, 2000);
+         */
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screen_width = displayMetrics.widthPixels;
+
+        logoImageTop = findViewById(R.id.top);
+        logoImageBottom = findViewById(R.id.bottom);
+
+        Animation animateImageTop = new TranslateAnimation(0, 0, -400, -150);
+        animateImageTop.setDuration(2000);
+        animateImageTop.setFillAfter(true);
+        logoImageTop.startAnimation(animateImageTop);
+
+        Animation animateImageBottom = new TranslateAnimation(0, 0, 800, 190);
+        animateImageBottom.setDuration(2000);
+        animateImageBottom.setFillAfter(true);
+        logoImageBottom.startAnimation(animateImageBottom);
+
+        animateImageBottom.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
 }
